75899b0f1a16be7d722280e272604a8b7fbbb57d
PhanTruongAn <phanan1000@gmail.com>
2024-06-27 05:57:51 +0700

    Create test model User with Sequelize

---
+++ back-end/src/seeders/20240626225125-demo-user.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("Users", [
+      {
+        firstName: "Phan",
+        lastName: "An",
+        email: "an123@gmail.com",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
