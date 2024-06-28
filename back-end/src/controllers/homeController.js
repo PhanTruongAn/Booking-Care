5f4bf9e107f1f9863b703e2b0f0ab5a718f72391
PhanTruongAn <phanan1000@gmail.com>
2024-06-28 07:57:25 +0700

    Create all model for project

--- back-end/src/controllers/homeController.js
+++ back-end/src/controllers/homeController.js
@@ -1,5 +1,14 @@
-let getHomePage = (req, res) => {
-  return res.render("homePage.ejs");
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    // console.log("---------------------------------------");
+    // console.log(data);
+    // console.log("---------------------------------------");
+    return res.render("homePage.ejs", { data: JSON.stringify(data) });
+  } catch (error) {
+    console.log(error);
+  }
 };
 module.exports = {
   getHomePage,
--- back-end/src/seeders/seeder-demo-user.js
+++ back-end/src/seeders/seeder-demo-user.js
@@ -5,9 +5,16 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("Users", [
       {
+        email: "admin@gmail.com",
+        password: "123",
         firstName: "Phan",
         lastName: "An",
-        email: "an123@gmail.com",
+        address: "F1/15 Ho Chi Minh",
+        phoneNumber: "0123456789",
+        gender: 1,
+        image: "link image",
+        roleId: "R1",
+        positionId: "P4",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
